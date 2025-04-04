bbd03f12751d03ad8f250dff62dbf7272bdd70e5
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-03 23:09:10 -0300

    :sparkles: feat: adicionar suporte à máscara de input com vue-the-mask

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
+import VueTheMask from 'vue-the-mask'
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueTheMask)
+
+app.mount('#app')
